9cea0407b50076f5d5c8ffefbbcf1a0203e0994c
Harry Perez <harryPerez14495@users.noreply.github.com>
2026-09-15 19:36:35 -0400

    Count nodes with at least one child

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - Counts nodes with at least one child
+ * @tree: Pointer to the root node of the tree
+ *
+ * Return: Number of nodes with at least one child
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
+
+	return (1 + binary_tree_nodes(tree->left) +
+		binary_tree_nodes(tree->right));
+}
